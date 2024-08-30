1edf7161245d35465198ca402cb19fe6456ac016
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-30 18:20:17 -0300

    test: ensure SUT creates a user

--- src/modules/users/__tests__/controllers/create-user.spec.ts
+++ src/modules/users/__tests__/controllers/create-user.spec.ts
@@ -1,8 +1,10 @@
 import { Test } from '@nestjs/testing';
 
+import { UserBuilder } from '../_data_/builders/user.builder';
 import { makeCreateUserDto } from '../_data_/factories/dtos/create-user.factory';
 
 import { CreateUserController } from '@/modules/users/presenters/controllers/create-user.controller';
+import { UserViewModel } from '@/modules/users/presenters/user.view-model';
 import { CreateUserUseCase } from '@/modules/users/use-cases/create-user.use-case';
 import { UserAlreadyExistsException } from '@/modules/users/use-cases/exceptions/user-already-exists.exception';
 
@@ -44,7 +46,25 @@ describe(CreateUserController.name, (): void => {
 		await expect(promise).rejects.toThrow(
 			UserAlreadyExistsException.byEmail(dto.email),
 		);
-		expect(useCase.exec).toHaveBeenNthCalledWith(1, dto);
+		expect(useCase.exec).toHaveBeenCalledWith(dto);
+	});
+
+	it('should create a user', async (): Promise<void> => {
+		const dto = makeCreateUserDto();
+
+		const user = new UserBuilder()
+			.withName(dto.name)
+			.withEmail(dto.email)
+			.withPassword(dto.password)
+			.withRole(dto.role)
+			.build();
+
+		jest.spyOn(useCase, 'exec').mockResolvedValueOnce({ user });
+
+		const result = await sut.handle(dto);
+
+		expect(useCase.exec).toHaveBeenCalledWith(dto);
+		expect(result).toEqual(UserViewModel.toJson(user));
 	});
 
 	afterAll((): void => {
