351a6b35778d955dd475d0b9696c142b62eacef0
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-30 12:05:09 -0300

    test: ensure SUT creates a new user

--- src/modules/users/__tests__/use-cases/create-user.use-case.spec.ts
+++ src/modules/users/__tests__/use-cases/create-user.use-case.spec.ts
@@ -4,6 +4,8 @@ import { Test } from '@nestjs/testing';
 import { CreateUserUseCaseBuilder } from '../_data_/builders/use-cases/create-user.builder';
 import { UserBuilder } from '../_data_/builders/user.builder';
 
+import { type ROLE } from '@/@core/consts/role';
+
 import { UsersRepository } from '@/modules/users/repositories/users.repository';
 import { HashingService } from '@/modules/users/services/hashing.service';
 import { CreateUserUseCase } from '@/modules/users/use-cases/create-user.use-case';
@@ -60,5 +62,32 @@ describe('CreateUserUseCase', () => {
 			UserAlreadyExistsException.byEmail(email),
 		);
 		expect(usersRepository.findByEmail).toHaveBeenNthCalledWith(1, email);
+	});
+
+	it('should create a new user', async (): Promise<void> => {
+		jest.spyOn(usersRepository, 'findByEmail').mockResolvedValueOnce(null);
+		jest
+			.spyOn(hashingService, 'hashString')
+			.mockResolvedValueOnce('my_hashed_password');
+
+		const email = 'john.doe@gmail.com';
+
+		const { user } = await sut.exec(
+			new CreateUserUseCaseBuilder()
+				.withName('John Doe')
+				.withEmail(email)
+				.withRole('ADMIN').props,
+		);
+
+		const { name, email: userEmail, role } = user.render();
+
+		expect(user).toBeDefined();
+		expect(name).toBe('John Doe');
+		expect(userEmail).toBe(email);
+		expect(role).toBe<ROLE>('ADMIN');
+	});
+
+	afterEach((): void => {
+		jest.clearAllMocks();
 	});
 });
