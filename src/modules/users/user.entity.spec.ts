bf36ad7c14337f0335a4185b0a44e08d0b47c0cb
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-30 11:04:00 -0300

    test: ensure SUT restores a UserEntity

--- src/modules/users/user.entity.spec.ts
+++ src/modules/users/user.entity.spec.ts
@@ -1,17 +1,31 @@
 import { UserEntity, type UserProps } from './user.entity';
 
 describe('UserEntity', (): void => {
-	it('should create a UserEntity', (): void => {
-		const userProps: UserProps = {
-			name: 'John Doe',
-			email: 'john.doe@gmail.com',
-			password: 'youshallnotpass',
-			role: 'ADMIN',
-		};
+	const userProps: UserProps = {
+		name: 'John Doe',
+		email: 'john.doe@gmail.com',
+		password: 'youshallnotpass',
+		role: 'ADMIN',
+	};
 
+	it('should create a UserEntity', (): void => {
 		const user = UserEntity.createNew(userProps);
 
 		expect(user).toBeDefined();
 		expect(user.render()).toStrictEqual(userProps);
+	});
+
+	it('should restore a UserEntity', (): void => {
+		const user = UserEntity.createNew(userProps);
+		const restoredUser = UserEntity.restore({
+			id: user.id,
+			props: {
+				...user.render(),
+			},
+		});
+
+		expect(user).toBeDefined();
+		expect(restoredUser).toBeDefined();
+		expect(restoredUser).toStrictEqual(user);
 	});
 });
