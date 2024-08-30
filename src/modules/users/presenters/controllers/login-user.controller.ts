4b7d180d498fee21e51a52ddf895f378f36dacf3
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-30 18:41:34 -0300

    refactor: create proper response type

--- src/modules/users/presenters/controllers/login-user.controller.ts
+++ src/modules/users/presenters/controllers/login-user.controller.ts
@@ -4,17 +4,29 @@ import { type LoginUserDto, LoginUserDtoBodyPipe } from './dtos/login-user.dto';
 
 import { type IController } from '@/@core/controller';
 
-import { UserViewModel } from '@/modules/users/presenters/user.view-model';
+import {
+	type UserToJson,
+	UserViewModel,
+} from '@/modules/users/presenters/user.view-model';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { LoginUserUseCase } from '@/modules/users/use-cases/login-user.use-case';
 
+type LoginUserControllerResponse = {
+	accessToken: string;
+	user: UserToJson;
+};
+
 @Controller('users/login')
-export class LoginUserController implements IController<any> {
+export class LoginUserController
+	implements IController<LoginUserControllerResponse>
+{
 	constructor(private readonly useCase: LoginUserUseCase) {}
 
 	@Post()
 	@HttpCode(HttpStatus.OK)
-	async handle(@Body(LoginUserDtoBodyPipe) body: LoginUserDto): Promise<any> {
+	async handle(
+		@Body(LoginUserDtoBodyPipe) body: LoginUserDto,
+	): Promise<LoginUserControllerResponse> {
 		const { accessToken, user } = await this.useCase.exec(body);
 
 		return {
