7a2cef740135db87a5028edb8b91a58555897b40
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-30 18:38:51 -0300

    test: ensure SUT returns an access token & the user on successful login

--- src/modules/users/__tests__/controllers/login-user.spec.ts
+++ src/modules/users/__tests__/controllers/login-user.spec.ts
@@ -1,8 +1,10 @@
 import { Test } from '@nestjs/testing';
 
+import { UserBuilder } from '../_data_/builders/user.builder';
 import { makeLoginUserDto } from '../_data_/factories/dtos/login-user.factory';
 
 import { LoginUserController } from '@/modules/users/presenters/controllers/login-user.controller';
+import { UserViewModel } from '@/modules/users/presenters/user.view-model';
 import { InvalidCredentialsException } from '@/modules/users/use-cases/exceptions/invalid-credentials.exception';
 import { LoginUserUseCase } from '@/modules/users/use-cases/login-user.use-case';
 
@@ -43,6 +45,28 @@ describe(LoginUserController.name, (): void => {
 
 		await expect(promise).rejects.toThrow(new InvalidCredentialsException());
 		expect(useCase.exec).toHaveBeenCalledWith(dto);
+	});
+
+	it('should return an access token and user on successful login', async () => {
+		const dto = makeLoginUserDto();
+		const user = new UserBuilder()
+			.withEmail(dto.email)
+			.withPassword(dto.password)
+			.build();
+		const accessToken = 'random_access_token';
+
+		jest.spyOn(useCase, 'exec').mockResolvedValueOnce({
+			accessToken,
+			user,
+		});
+
+		const result = await sut.handle(dto);
+
+		expect(useCase.exec).toHaveBeenCalledWith(dto);
+		expect(result).toStrictEqual({
+			accessToken,
+			user: UserViewModel.toJson(user),
+		});
 	});
 
 	afterAll((): void => {
