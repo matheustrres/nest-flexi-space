ca5a35208e152945a90954a4c7d5766e8bb05921
Matheus Torres <torresmiranda.rj@gmail.com>
2024-09-02 09:58:24 -0300

    test: ensure SUT restores a RoomEntity

--- src/modules/rooms/room.entity.spec.ts
+++ src/modules/rooms/room.entity.spec.ts
@@ -1,13 +1,15 @@
-import { RoomEntity } from './room.entity';
+import { type OptionalRoomConstructorProps, RoomEntity } from './room.entity';
 
 describe(RoomEntity.name, (): void => {
+	const roomProps: OptionalRoomConstructorProps = {
+		name: 'Mega Room',
+		description: 'Room description',
+		capacity: 10,
+		location: 'Room location',
+	};
+
 	it('should create a RoomEntity', (): void => {
-		const room = RoomEntity.createNew({
-			name: 'Mega Room',
-			description: 'Room description',
-			capacity: 10,
-			location: 'Room location',
-		});
+		const room = RoomEntity.createNew(roomProps);
 
 		const { name, slug, isReserved } = room.render();
 
@@ -15,5 +17,17 @@ describe(RoomEntity.name, (): void => {
 		expect(name).toBe('Mega Room');
 		expect(slug.props.value).toBe('mega-room');
 		expect(isReserved).toBe(false);
+	});
+
+	it('should restore a RoomEntity', (): void => {
+		const room = RoomEntity.createNew(roomProps);
+		const restoredRoom = RoomEntity.restore({
+			id: room.id,
+			props: room.render(),
+		});
+
+		expect(room).toBeDefined();
+		expect(restoredRoom).toBeDefined();
+		expect(restoredRoom).toStrictEqual(room);
 	});
 });
