7a7d40bbfd30f791a63c53e3552d359b69fdf82a
Matheus Torres <torresmiranda.rj@gmail.com>
2024-09-02 09:56:22 -0300

    feat: create RoomEntity

--- src/@core/types/index.ts
+++ src/@core/types/index.ts
@@ -5,3 +5,5 @@ export type JwtPayload = {
 	sub: string;
 	role: ROLE;
 };
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> &
+	Omit<T, K>;
---
+++ src/modules/rooms/room.entity.ts
@@ -0,0 +1,43 @@
+import { type CreateEntityProps, Entity } from '@/@core/entity';
+import { EntityId } from '@/@core/entity-id';
+import { type Optional } from '@/@core/types';
+import { Slug } from '@/@core/value-objects/slug';
+
+export type RoomProps = {
+	name: string;
+	slug: Slug;
+	description: string;
+	location: string;
+	capacity: number;
+	isReserved: boolean;
+	availability: (Date | string)[];
+};
+
+export type RoomConstructorProps = CreateEntityProps<RoomProps>;
+
+export type OptionalRoomConstructorProps = Optional<
+	RoomProps,
+	'availability' | 'isReserved' | 'slug'
+>;
+
+export class RoomEntity extends Entity<RoomProps> {
+	private constructor(props: RoomConstructorProps) {
+		super(props);
+	}
+
+	static createNew(props: OptionalRoomConstructorProps): RoomEntity {
+		return new RoomEntity({
+			id: new EntityId(),
+			props: {
+				...props,
+				availability: props.availability ?? [],
+				isReserved: props.isReserved ?? false,
+				slug: props.slug ?? Slug.createFromText(props.name),
+			},
+		});
+	}
+
+	static restore(props: RoomConstructorProps): RoomEntity {
+		return new RoomEntity(props);
+	}
+}
